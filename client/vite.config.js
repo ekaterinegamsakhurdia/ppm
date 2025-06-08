4e5d89ff32c4090fa4651a5ad70e8a5b6b8e3d28
ekaterinegamsakhurdia <gamsakhurdia.ekateri@kiu.edu.ge>
2025-06-08 23:12:59 +0400

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Ensures assets are loaded from root
+});
